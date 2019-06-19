3fc8b252b0b0e57929f9410b164fc62d65aba041
maks0667966380@gmail.com <maks0667966380@gmail.com>
2019-06-19 03:37:37 +0300

    2-connection list

--- ExamProject/ExamProject/main.cpp
+++ ExamProject/ExamProject/main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>
+#include "List.h"
 
-using namespace sf;
+using namespace std;
 
 int main()
 {
-	RenderWindow window(VideoMode(800, 600), "Visual sort");
-	window.display();
-	std::cout << "Hello, world!" << std::endl;
+	List<int> a;
+	a.set(1, 2);
+	a.set(1, 3);
+	a.set(42, 42);
+	a.set(-42, -42);
+	cout << a << endl;
+	cout << a.get(0) << endl;
+	cout << a.get(-1) << endl;
+	//cout << a.get(42) << endl;
+	a[1] = a[2];
+	cout << a[1] << endl;
+	//cout << a[42] << endl;
+	cout << a << endl;
+	a.insert(0, 0);
+	a.insert(a.length() - 1, a.length() - 1);
+	a.insert(1, 1);
+	cout << a << endl;
+	a.remove(1);
+	a.remove(a.length() - 1);
+	a.remove(0);
+	cout << a << endl;
+	cout << "That's all Folks!" << endl;
+	cin.get();
 	return 0;
 }
