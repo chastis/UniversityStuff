46da99a1fd6ecefb7ed70cae03f6b86cf2d7da4e
Maxi Shatohin <maks0667966380@gmail.com>
2020-06-14 20:25:19 +0300

    clock algorithm

--- SixthSemester/SPOS/MemoryManagment/PageFault.java
+++ SixthSemester/SPOS/MemoryManagment/PageFault.java
@@ -50,35 +50,37 @@ public class PageFault {
    * @param controlPanel represents the graphical element of the 
    *   simulator, and allows one to modify the current display.
    */
+
+  int ClockArrow = 0;
+
   public static void replacePage ( Vector mem , int virtPageNum , int replacePageNum , ControlPanel controlPanel ) 
   {
     int count = 0;
     int oldestPage = -1;
-    int oldestTime = 0;
-    int firstPage = -1;
-    int map_count = 0;
     boolean mapped = false;
 
-    while ( ! (mapped) || count != virtPageNum ) {
+    while (!mapped)
+    {
+      if (count == virtPageNum)
+      {
+        count = 0;
+      }
       Page page = ( Page ) mem.elementAt( count );
-      if ( page.physical != -1 ) {
-        if (firstPage == -1) {
-          firstPage = count;
-        }
-        if (page.inMemTime > oldestTime) {
-          oldestTime = page.inMemTime;
+      if ( page.physical != -1 )
+      {
+        if (page.R == 0)
+        {
           oldestPage = count;
           mapped = true;
         }
+        else
+        {
+          page.R = 0;
+        }
       }
       count++;
-      if ( count == virtPageNum ) {
-        mapped = true;
-      }
     }
-    if (oldestPage == -1) {
-      oldestPage = firstPage;
-    }
+    System.out.println( "We replace :" + oldestPage + " to " + replacePageNum );
     Page page = ( Page ) mem.elementAt( oldestPage );
     Page nextpage = ( Page ) mem.elementAt( replacePageNum );
     controlPanel.removePhysicalPage( oldestPage );
